d8a29b9f29d762d1ab61973c1626486b13946b60
alisson manuela caicedo cardona <manucaicedo1205@gmail.com>
2023-05-17 20:05:33 -0500

    creacion del proyecto con componenetes

---
+++ src/componentes/Navegador.jsx
@@ -0,0 +1,16 @@
+const Navegador = () =>{
+    return(
+        <section>
+            <nav>
+                <ul>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/Dulces'}>Dulces</Link>
+                    <Link to={'/Peluches'}>Productos</Link>
+                   
+    
+                </ul>
+            </nav>
+            </section>
+            )
+            }
+export default Navegador
