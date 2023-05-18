d0d8481d2b536c4069857b0576469618de6b2e27
alisson manuela caicedo cardona <manucaicedo1205@gmail.com>
2023-05-18 18:30:14 -0500

    cree el json de dulces

--- src/componentes/Dulces.jsx
+++ src/componentes/Dulces.jsx
@@ -2,9 +2,14 @@ import Navegador from "./Navegador"
 const Dulces = () => {
     return(
         <section>
+
             <Navegador/> 
+            <section>
+                
+            </section>
 
         </section>
+       
     )
 }
  export default Dulces
